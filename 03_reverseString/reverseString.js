bbfdca8508431efa6c9e3925b5c71b3d248d5ce4
Tomasz Wasik <tomaszwasik31@gmail.com>
2022-07-05 15:40:27 +0100

    exercise 3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
 
+    //split string to create arr
+    const splitString = str.split("");
+
+    
+    const reverseArray = splitString.reverse();
+    const joinArray = reverseArray.join("");
+    return joinArray;  
 };
 
 // Do not edit below this line
