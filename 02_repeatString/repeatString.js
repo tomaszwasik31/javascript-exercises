f94ad024f8dc5a6323a5122a7ad161828232a0da
Tomasz Wasik <tomaszwasik31@gmail.com>
2022-07-05 15:26:29 +0100

    2 exercise done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function (word, repeats) {
+  let result = "";
 
-};
+  if(repeats<0){
+    result = "ERROR"
+  } 
+  for (let i = 0; i<repeats;i++){
+    result += `${word}`;
+  }
+  return result;
+};  
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
