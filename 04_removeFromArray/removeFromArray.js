56e362aaf55c0af29df15ca1a777c93637d3a57b
Tomasz Wasik <tomaszwasik31@gmail.com>
2022-07-08 16:04:19 +0100

    exercise 4 'done'

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function (...args) {
+  // add additional args to array
 
+  const array = args[0];
+  const newArray = [];
+  console.log(array);
+
+  array.forEach((item) => {
+    // push every element into the new array
+    // UNLESS it is included in the function arguments
+    // so we create a new array with every item, except those that should be removed
+    if (!args.includes(item)) {
+      newArray.push(item);
+      console.log(newArray);
+    }
+  });
+  // and return that array
+  return newArray;
 };
 
 // Do not edit below this line
