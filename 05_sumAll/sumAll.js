968ba60e95a6a34a4f3017c45d6223e9cb9c90df
Tomasz Wasik <tomaszwasik31@gmail.com>
2022-07-11 14:30:03 +0100

    exercise 5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function (firstNumber, secondNumber) {
+  
 
+  let smallerNumber = firstNumber;
+  let largerNumber = secondNumber;
+  let sum = 0;
+
+  // if larger number first
+  if (firstNumber > secondNumber) {
+    largerNumber = firstNumber;
+    smallerNumber = secondNumber;
+  }
+
+  // if negative number ERROR
+
+  if (firstNumber < 0 || secondNumber < 0) {
+    return "ERROR";
+  }
+
+  //if NaN
+  if (!Number.isFinite(firstNumber) || !Number.isFinite(secondNumber)) {
+    return "ERROR";
+  }
+
+  // sum number between
+  for (let i = smallerNumber; i <= largerNumber; i++) {
+    sum += i;
+  }
+  console.log(sum);
+  return sum;
 };
 
 // Do not edit below this line
