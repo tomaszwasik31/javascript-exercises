df112a3e61b8f3b26ccb2c40c13d25a6d536bd81
Tomasz Wasik <tomaszwasik31@gmail.com>
2022-07-11 18:22:47 +0100

    exercise 8 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -24,9 +24,22 @@ let multiply = 1;
   return multiply;
 };
 
-const power = function () {};
+const power = function (a,b) {
+  return Math.pow(a, b);
+};
 
-const factorial = function () {};
+const factorial = function (n) {
+//copied code
+  let answer = 1;
+  if (n == 0 || n == 1){
+    return answer;
+  }else{
+    for(let i = n; i >= 1; i--){
+      answer *= i;
+    }
+    return answer;
+  }  
+};
 
 // Do not edit below this line
 module.exports = {
