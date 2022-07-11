332ee5c2954f41b4b828be85a16d7fde4cb5b85d
Tomasz Wasik <tomaszwasik31@gmail.com>
2022-07-11 17:37:36 +0100

    exercise 7 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const ftoc = function() {
+const ftoc = function(fahrenheit) {
 
+  let celsius = (fahrenheit-32)/1.8;
+  let rounded = Math.round(celsius * 10) / 10;
+  return rounded;
 };
 
-const ctof = function() {
+const ctof = function(celsius) {
+
+  let fahrenheit = (celsius*1.8000) + 32.00;
+  let rounded = Math.round(fahrenheit * 10) / 10;
+  return rounded;
 
 };
 
