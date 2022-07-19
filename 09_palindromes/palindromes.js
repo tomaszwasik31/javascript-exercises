1b1621097cd9de8a346601fda6b1ff98abcb5e7e
Tomasz Wasik <tomaszwasik31@gmail.com>
2022-07-19 18:42:02 +0100

    palidrome base functions

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let arr = Array.from(string);
 
+  function checkPunctuation(a) {
+    return a !== "!";
+  }
+
+  if (
+    arr.filter(checkPunctuation).join("") ===
+    arr.reverse().filter(checkPunctuation).join("")
+  ) {
+    return true;
+  }
 };
 
 // Do not edit below this line
