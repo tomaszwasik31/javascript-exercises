b0425ebbfab080364ecea456b00ce98fb9dde270
Tomasz Wasik <tomaszwasik31@gmail.com>
2022-07-20 16:12:30 +0100

    start coding, testing

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  //no death date
 
+  const currentYear = new Date().getFullYear();
+
+ let hasDeathYear = arr.hasOwnProperty('yearOfDeath');
+
+ if (hasDeathYear){
+    console.log('yes');
+ }else{
+    console.log('no');
+    arr['yearOfDeath'] = currentYear;
+ }
+
+  const oldestSorting = arr.sort(function (a, b) {
+    const lastPerson = a.yearOfDeath - a.yearOfBirth;
+    const nextPerson = b.yearOfDeath - b.yearOfBirth;
+    return lastPerson > nextPerson ? -1 : 1;
+  });
+  console.log(oldestSorting);
+  return oldestSorting[0];
 };
 
 // Do not edit below this line
