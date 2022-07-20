4f3b34daf4a1c92ed68de6d4a2d2e3fa681ea069
Tomasz Wasik <tomaszwasik31@gmail.com>
2022-07-20 12:09:47 +0100

    exercise done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  n = Number(n);
+  let a = 0;
+  let b = 1;
+  let c = n;
 
+  if (n < 0) {
+    return "OOPS";
+  }
+
+  for (let i = 2; i <= n; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+  return c;
 };
 
 // Do not edit below this line
